e697336676b8fe76395d6c3f65fdab8d95e5f447
herdo dimas pratirto <herdodimas46@gmail.com>
2024-09-19 11:25:05 +0700

    input/output dan komentar

---
+++ learn2.py
@@ -0,0 +1,11 @@
+#input/Output dan komentar
+nama = input("masukkan nama anda : ")
+print(nama)
+
+#komentar ada 2 jenis : inline comment dan block comment
+#ini adalah contoh inline comment
+
+#dibawah ini adalah contoh block comment
+"""
+ini contoh block comment. teks ini tidak akan dieksekusi_
+"""
